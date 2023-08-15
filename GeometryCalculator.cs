b020a3cade3ecab5a73901dd4114ce8cfff01d33
AlexeyLVB <korolevaleksej12@gmail.com>
2023-08-15 16:06:04 +0300

    first commit

---
+++ GeometryCalculator.cs
@@ -0,0 +1,31 @@
+﻿namespace GeometryLibrary
+{
+    public interface IShape
+    {
+        double CalculateArea();
+    }
+
+    public class Circle : IShape
+    {
+        public double Radius { get; set; }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+
+    public class Triangle : IShape
+    {
+        public double Side1 { get; set; }
+        public double Side2 { get; set; }
+        public double Side3 { get; set; }
+
+        public double CalculateArea()
+        {
+            double semiPerimeter = (Side1 + Side2 + Side3) / 2;
+            double area = Math.Sqrt(semiPerimeter * (semiPerimeter - Side1) * (semiPerimeter - Side2) * (semiPerimeter - Side3));
+            return area;
+        }
+    }
+}
